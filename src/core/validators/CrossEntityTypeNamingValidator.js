38c46f8477d5017c30b614efca238158baa7a005
lanierhall-doubleline <lanier.hall@doublelinepartners.com>
2016-08-23 17:18:48 -0500

    squash! cetnv

--- src/core/validators/CrossEntityTypeNamingValidator.js
+++ src/core/validators/CrossEntityTypeNamingValidator.js
@@ -1,17 +1,3 @@
-using;
-System.Collections.Generic;
-using;
-Antlr4.Runtime;
-using;
-Antlr4.Runtime.Tree;
-using;
-MetaEd.Common.Services;
-using;
-MetaEd.Core.Tasks;
-using;
-MetaEd.Grammar.Antlr;
-using;
-MetaEd.Grammar.Antlr.Extensions;
 var MetaEd;
 (function (MetaEd) {
     var Core;
@@ -19,63 +5,37 @@ var MetaEd;
         var Validator;
         (function (Validator) {
             class CrossEntityTypeNamingValidator {
-            }
-            ISymbolTableBuilderListener;
-            {
-                readonly;
-                ISet < string > _trackedEntityNames;
-                new HashSet();
-                IMetaEdContext;
-                _context;
-                void WithContext(IMetaEdContext, context);
-                {
-                    _context = context;
+                constructor() {
+                    this._trackedEntityNames = new HashSet();
                 }
-                bool;
-                BeforeAddEntity(string, candidateEntityType, ITerminalNode, candidateEntityName, ParserRuleContext, candidateContext);
-                {
-                    // MetaEd extension entities don't define a new identifier
+                withContext(context) {
+                    this._context = context;
+                }
+                beforeAddEntity(candidateEntityType, candidateEntityName, candidateContext) {
                     if (candidateContext.IsExtensionContext())
                         return true;
-                    // Domains, Subdomains, Interchanges, enumerations and descriptors don't have standard cross entity naming issues
-                    if (candidateContext)
-                        is;
-                    MetaEdGrammar.DomainContext ||
-                        candidateContext;
-                    is;
-                    MetaEdGrammar.SubdomainContext ||
-                        candidateContext;
-                    is;
-                    MetaEdGrammar.InterchangeContext ||
-                        candidateContext;
-                    is;
-                    MetaEdGrammar.EnumerationContext ||
-                        candidateContext;
-                    is;
-                    MetaEdGrammar.DescriptorContext;
-                    return true;
-                    if (_trackedEntityNames.Contains(candidateEntityName.ToString())) {
-                        var metaEdFile = _context.MetaEdFileIndex.GetFileAndLineNumber(candidateEntityName.Symbol.Line);
-                        var failure = new ValidationMessage;
-                        {
-                            Message = string.Format("{0} named {1} is a duplicate declaration of that name.", candidateEntityType, candidateEntityName),
-                                CharacterPosition = candidateEntityName.Symbol.Column,
-                                ConcatenatedLineNumber = candidateEntityName.Symbol.Line,
-                                FileName = metaEdFile.FileName,
-                                LineNumber = metaEdFile.LineNumber;
-                        }
-                        ;
-                        _context.ErrorMessageCollection.Add(failure);
+                    if (candidateContext instanceof MetaEdGrammar.DomainContext || candidateContext instanceof MetaEdGrammar.SubdomainContext || candidateContext instanceof MetaEdGrammar.InterchangeContext || candidateContext instanceof MetaEdGrammar.EnumerationContext || candidateContext instanceof MetaEdGrammar.DescriptorContext)
+                        return true;
+                    if (this._trackedEntityNames.Contains(candidateEntityName.ToString())) {
+                        var metaEdFile = this._context.MetaEdFileIndex.GetFileAndLineNumber(candidateEntityName.Symbol.Line);
+                        var failure = __init(new ValidationMessage(), {
+                            Message: string.Format("{0} named {1} is a duplicate declaration of that name.", candidateEntityType, candidateEntityName),
+                            CharacterPosition: candidateEntityName.Symbol.Column,
+                            ConcatenatedLineNumber: candidateEntityName.Symbol.Line,
+                            FileName: metaEdFile.FileName,
+                            LineNumber: metaEdFile.LineNumber
+                        });
+                        this._context.ErrorMessageCollection.Add(failure);
                         return false;
                     }
-                    _trackedEntityNames.Add(candidateEntityName.ToString());
+                    this._trackedEntityNames.Add(candidateEntityName.ToString());
                     return true;
                 }
-                void Reset();
-                {
-                    _trackedEntityNames.Clear();
+                reset() {
+                    this._trackedEntityNames.Clear();
                 }
             }
+            Validator.CrossEntityTypeNamingValidator = CrossEntityTypeNamingValidator;
         })(Validator = Core.Validator || (Core.Validator = {}));
     })(Core = MetaEd.Core || (MetaEd.Core = {}));
 })(MetaEd || (MetaEd = {}));
--- src/core/validators/CrossEntityTypeNamingValidator.ts
+++ src/core/validators/CrossEntityTypeNamingValidator.ts
@@ -1,57 +1,32 @@
-﻿using System.Collections.Generic;
-using Antlr4.Runtime;
-using Antlr4.Runtime.Tree;
-using MetaEd.Common.Services;
-using MetaEd.Core.Tasks;
-using MetaEd.Grammar.Antlr;
-using MetaEd.Grammar.Antlr.Extensions;
-
-namespace MetaEd.Core.Validator
-{
-    public class CrossEntityTypeNamingValidator : ISymbolTableBuilderListener
-    {
-        private readonly ISet<string> _trackedEntityNames = new HashSet<string>();
-        private IMetaEdContext _context;
-
-        public void WithContext(IMetaEdContext context)
-        {
-            _context = context;
+﻿module MetaEd.Core.Validator {
+    export class CrossEntityTypeNamingValidator implements ISymbolTableBuilderListener {
+        private _trackedEntityNames: ISet<string> = new HashSet<string>();
+        private _context: IMetaEdContext;
+        public withContext(context: IMetaEdContext): void {
+            this._context = context;
         }
-
-        public bool BeforeAddEntity(string candidateEntityType, ITerminalNode candidateEntityName, ParserRuleContext candidateContext)
-        {
-            // MetaEd extension entities don't define a new identifier
-            if (candidateContext.IsExtensionContext()) return true;
-
-            // Domains, Subdomains, Interchanges, enumerations and descriptors don't have standard cross entity naming issues
-            if (candidateContext is MetaEdGrammar.DomainContext ||
-                candidateContext is MetaEdGrammar.SubdomainContext ||
-                candidateContext is MetaEdGrammar.InterchangeContext ||
-                candidateContext is MetaEdGrammar.EnumerationContext ||
-                candidateContext is MetaEdGrammar.DescriptorContext) return true;
-
-            if (_trackedEntityNames.Contains(candidateEntityName.ToString()))
-            {
-                var metaEdFile = _context.MetaEdFileIndex.GetFileAndLineNumber(candidateEntityName.Symbol.Line);
-                var failure = new ValidationMessage
-                                    {
-                                        Message = string.Format("{0} named {1} is a duplicate declaration of that name.", candidateEntityType, candidateEntityName),
-                                        CharacterPosition = candidateEntityName.Symbol.Column,
-                                        ConcatenatedLineNumber = candidateEntityName.Symbol.Line,
-                                        FileName = metaEdFile.FileName,
-                                        LineNumber = metaEdFile.LineNumber
-                                    };
-                _context.ErrorMessageCollection.Add(failure);
+        public beforeAddEntity(candidateEntityType: string, candidateEntityName: ITerminalNode, candidateContext: ParserRuleContext): boolean {
+            if (candidateContext.IsExtensionContext())
+                return true;
+            if (candidateContext instanceof MetaEdGrammar.DomainContext || candidateContext instanceof MetaEdGrammar.SubdomainContext || candidateContext instanceof MetaEdGrammar.InterchangeContext || candidateContext instanceof MetaEdGrammar.EnumerationContext || candidateContext instanceof MetaEdGrammar.DescriptorContext)
+                return true;
+            if (this._trackedEntityNames.Contains(candidateEntityName.ToString())) {
+                var metaEdFile = this._context.MetaEdFileIndex.GetFileAndLineNumber(candidateEntityName.Symbol.Line);
+                var failure = __init(new ValidationMessage(), {
+                    Message: string.Format("{0} named {1} is a duplicate declaration of that name.", candidateEntityType, candidateEntityName),
+                    CharacterPosition: candidateEntityName.Symbol.Column,
+                    ConcatenatedLineNumber: candidateEntityName.Symbol.Line,
+                    FileName: metaEdFile.FileName,
+                    LineNumber: metaEdFile.LineNumber
+                });
+                this._context.ErrorMessageCollection.Add(failure);
                 return false;
             }
-
-            _trackedEntityNames.Add(candidateEntityName.ToString());
+            this._trackedEntityNames.Add(candidateEntityName.ToString());
             return true;
         }
-
-        public void Reset()
-        {
-            _trackedEntityNames.Clear();
+        public reset(): void {
+            this._trackedEntityNames.Clear();
         }
     }
 }
