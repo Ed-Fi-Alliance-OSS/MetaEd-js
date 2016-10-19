a00b75803fd346e26dfff0eac685acaf256a8b8e
Brad Banister <brad@doublelinepartners.com>
2016-10-19 15:43:13 -0500

    all association extension tests

--- src/core/validators/AssociationExtension/AssociationExtensionMustNotDuplicateAssociationPropertyName.js
+++ src/core/validators/AssociationExtension/AssociationExtensionMustNotDuplicateAssociationPropertyName.js
@@ -1,39 +1,32 @@
-import {ValidationRuleBase} from "../ValidationRuleBase";
-import {ISymbolTable} from '../SymbolTable'
-import SymbolTableEntityType from "../SymbolTableEntityType";
-import {MetaEdGrammar} from '../../../../src/grammar/gen/MetaEdGrammar';
+// @flow
+import { MetaEdGrammar } from '../../../../src/grammar/gen/MetaEdGrammar';
+import { associationExtensionErrorRule, includeAssociationExtensionRule } from './AssociationExtensionValidationRule';
+import type SymbolTable from '../SymbolTable';
 
-export class AssociationExtensionMustNotDuplicateAssociationPropertyName extends ValidationRuleBase
-{
-    private symbolTable: ISymbolTable;
-    private symbolTableEntityType: SymbolTableEntityType = new SymbolTableEntityType();
-    constructor(symbolTable: ISymbolTable) {
-        super();
-        this.symbolTable = symbolTable;
-    }
-
-    public handlesContext(context: any) : boolean {
-        return context.ruleIndex === MetaEdGrammar.RULE_associationExtension;
-    }
-
-    public isValid(context: any): boolean {
-        return this.propertyRuleContextsForDuplicates(context).length == 0;
-    }
-    public getFailureMessage(context: any): string {
-        let duplicatePropertyIdentifierList = this.propertyRuleContextsForDuplicates(context).map(x => x.propertyName().ID().getText());
-        return `Association additions '${context.extendeeName().getText()}' declares '${duplicatePropertyIdentifierList.join(',')}' already in property list of Association.`;
-    }
-    protected propertyRuleContextsForDuplicates(context: any): Array<any> {
-        let entityType = context.ASSOCIATION().getText();
-        let extensionType = context.ASSOCIATION().getText() + context.ADDITIONS().getText();
-        let identifier = context.extendeeName().getText();
-        let associationPropertyIdentifiers = this.symbolTable.identifiersForEntityProperties(entityType, identifier);
-        let duplicates = this.symbolTable.contextsForMatchingPropertyIdentifiers(extensionType, identifier, Array.from(associationPropertyIdentifiers));
-        return duplicates.filter(x => this.isNotIncludePropertyContextWithExtension(x));
-    }
-
-    private isNotIncludePropertyContextWithExtension(context: any): boolean {
-        if (context.ruleIndex !== MetaEdGrammar.RULE_includeProperty) return true;
-        return context.includeExtensionOverride() === null;
-    }
+function isNotIncludePropertyContextWithExtension(context: any): boolean {
+  if (context.ruleIndex !== MetaEdGrammar.RULE_includeProperty) return true;
+  return context.includeExtensionOverride() === null;
 }
+
+function propertyRuleContextsForDuplicates(context: any, symbolTable: SymbolTable): Array<any> {
+  const entityType = context.ASSOCIATION().getText();
+  const extensionType = context.ASSOCIATION().getText() + context.ADDITIONS().getText();
+  const identifier = context.extendeeName().getText();
+  const associationPropertyIdentifiers = symbolTable.identifiersForEntityProperties(entityType, identifier);
+  const duplicates = symbolTable.contextsForMatchingPropertyIdentifiers(extensionType, identifier, Array.from(associationPropertyIdentifiers));
+  return duplicates.filter(x => isNotIncludePropertyContextWithExtension(x));
+}
+
+function valid(ruleContext: any, symbolTable: SymbolTable): boolean {
+  return propertyRuleContextsForDuplicates(ruleContext, symbolTable).length === 0;
+}
+
+function failureMessage(ruleContext: any, symbolTable: SymbolTable): string {
+  const duplicatePropertyIdentifierList = propertyRuleContextsForDuplicates(ruleContext, symbolTable).map(x => x.propertyName().ID().getText());
+  return `Association additions '${ruleContext.extendeeName().getText()}' declares '${duplicatePropertyIdentifierList.join(',')}' already in property list of Association.`;
+}
+
+const validationRule = associationExtensionErrorRule(valid, failureMessage);
+export { validationRule as default };
+
+export const includeRule = includeAssociationExtensionRule(validationRule);
--- test/core/validators/AssociationExtension/AssociationExtensionMustNotDuplicateAssociationPropertyNameTests.js
+++ test/core/validators/AssociationExtension/AssociationExtensionMustNotDuplicateAssociationPropertyNameTests.js
@@ -1,36 +1,36 @@
-import MetaEdTextBuilder from "../../../grammar/MetaEdTextBuilder";
-import chai from 'chai'
-import ValidatorTestHelper from "../ValidatorTestHelper";
-import ValidatorListener from "../../../../src/core/validators/ValidatorListener";
-import TestRuleProvider from "../TestRuleProvider";
-import SymbolTable from "../../../../src/core/validators/SymbolTable";
-import {AssociationExtensionMustNotDuplicateAssociationPropertyName} from "../../../../src/core/validators/AssociationExtension/AssociationExtensionMustNotDuplicateAssociationPropertyName";
+import chai from 'chai';
+import MetaEdTextBuilder from '../../../grammar/MetaEdTextBuilder';
+import ValidatorTestHelper from '../ValidatorTestHelper';
+import ValidatorListener from '../../../../src/core/validators/ValidatorListener';
+import { newRepository } from '../../../../src/core/validators/ValidationRuleRepository';
+import SymbolTable from '../../../../src/core/validators/SymbolTable';
+import { includeRule } from '../../../../src/core/validators/AssociationExtension/AssociationExtensionMustNotDuplicateAssociationPropertyName';
 
-import {MetaEdGrammar} from '../../../../src/grammar/gen/MetaEdGrammar';
-let should = chai.should();
+chai.should();
 
 describe('AssociationExtensionMustNotDuplicateAssociationPropertyName', () => {
+  const repository = includeRule(newRepository());
+  const validatorListener = new ValidatorListener(repository);
+
   describe('When_association_extension_has_different_property_name', () => {
     const symbolTable = new SymbolTable();
-    const validatorListener = new ValidatorListener(
-      new TestRuleProvider(MetaEdGrammar.RULE_associationExtension, new AssociationExtensionMustNotDuplicateAssociationPropertyName(symbolTable)));
-
-    let entityName: string = "MyIdentifier";
-    let helper: ValidatorTestHelper = new ValidatorTestHelper();
+    const entityName: string = 'MyIdentifier';
+    const helper: ValidatorTestHelper = new ValidatorTestHelper();
     before(() => {
-      let metaEdText = MetaEdTextBuilder.build()
-      .withBeginNamespace("edfi")
+      const metaEdText = MetaEdTextBuilder.build()
+      .withBeginNamespace('edfi')
       .withStartAssociation(entityName)
-      .withDocumentation("because documentation is required")
-      .withDomainEntityProperty("DomainEntity1", "doc")
-      .withDomainEntityProperty("DomainEntity2", "doc")
-      .withBooleanProperty("Property1", "because a property is required", true, false)
+      .withDocumentation('because documentation is required')
+      .withDomainEntityProperty('DomainEntity1', 'doc')
+      .withDomainEntityProperty('DomainEntity2', 'doc')
+      .withBooleanProperty('Property1', 'because a property is required', true, false)
       .withEndAssociation()
 
       .withStartAssociationExtension(entityName)
-      .withBooleanProperty("Property2", "because a property is required", true, false)
+      .withBooleanProperty('Property2', 'because a property is required', true, false)
       .withEndAssociationExtension()
-      .withEndNamespace().toString();
+      .withEndNamespace()
+      .toString();
       helper.setup(metaEdText, validatorListener, symbolTable);
     });
 
@@ -41,105 +41,101 @@ describe('AssociationExtensionMustNotDuplicateAssociationPropertyName', () => {
 
   describe('When_association_extension_has_duplicate_property_name', () => {
     const symbolTable = new SymbolTable();
-    const validatorListener = new ValidatorListener(
-      new TestRuleProvider(MetaEdGrammar.RULE_associationExtension, new AssociationExtensionMustNotDuplicateAssociationPropertyName(symbolTable)));
-
-    let entityName: string = "MyIdentifier";
-    const duplicatePropertyName: string = "Property1";
-    let helper: ValidatorTestHelper = new ValidatorTestHelper();
+    const entityName: string = 'MyIdentifier';
+    const duplicatePropertyName: string = 'Property1';
+    const helper: ValidatorTestHelper = new ValidatorTestHelper();
     before(() => {
-      let metaEdText = MetaEdTextBuilder.build()
-      .withBeginNamespace("edfi")
+      const metaEdText = MetaEdTextBuilder.build()
+      .withBeginNamespace('edfi')
       .withStartAssociation(entityName)
-      .withDocumentation("because documentation is required")
-      .withDomainEntityProperty("DomainEntity1", "doc")
-      .withDomainEntityProperty("DomainEntity2", "doc")
-      .withBooleanProperty(duplicatePropertyName, "because a property is required", true, false)
+      .withDocumentation('because documentation is required')
+      .withDomainEntityProperty('DomainEntity1', 'doc')
+      .withDomainEntityProperty('DomainEntity2', 'doc')
+      .withBooleanProperty(duplicatePropertyName, 'because a property is required', true, false)
       .withEndAssociation()
 
       .withStartAssociationExtension(entityName)
-      .withBooleanProperty(duplicatePropertyName, "because a property is required", true, false)
+      .withBooleanProperty(duplicatePropertyName, 'because a property is required', true, false)
       .withEndAssociationExtension()
-      .withEndNamespace().toString();
+      .withEndNamespace()
+      .toString();
       helper.setup(metaEdText, validatorListener, symbolTable);
     });
 
     it('should_have_validation_failure()', () => {
       helper.errorMessageCollection.length.should.equal(1);
     });
+
     it('should_have_validation_failure_message()', () => {
-      helper.errorMessageCollection[0].message.should.include("Association additions");
+      helper.errorMessageCollection[0].message.should.include('Association additions');
       helper.errorMessageCollection[0].message.should.include(entityName);
       helper.errorMessageCollection[0].message.should.include(duplicatePropertyName);
-      helper.errorMessageCollection[0].message.should.include("already in property list");
+      helper.errorMessageCollection[0].message.should.include('already in property list');
     });
   });
 
   describe('When_association_extension_has_multiple_association_names', () => {
     const symbolTable = new SymbolTable();
-    const validatorListener = new ValidatorListener(
-      new TestRuleProvider(MetaEdGrammar.RULE_associationExtension, new AssociationExtensionMustNotDuplicateAssociationPropertyName(symbolTable)));
-
-    let entityName: string = "MyIdentifier";
-    const notDuplicatePropertyName: string = "NotADuplicate";
-    const duplicatePropertyName1: string = "Property1";
-    const duplicatePropertyName2: string = "Property2";
-    let helper: ValidatorTestHelper = new ValidatorTestHelper();
+    const entityName: string = 'MyIdentifier';
+    const notDuplicatePropertyName: string = 'NotADuplicate';
+    const duplicatePropertyName1: string = 'Property1';
+    const duplicatePropertyName2: string = 'Property2';
+    const helper: ValidatorTestHelper = new ValidatorTestHelper();
     before(() => {
-      let metaEdText = MetaEdTextBuilder.build()
-      .withBeginNamespace("edfi")
+      const metaEdText = MetaEdTextBuilder.build()
+      .withBeginNamespace('edfi')
       .withStartAssociation(entityName)
-      .withDocumentation("because documentation is required")
-      .withDomainEntityProperty("DomainEntity1", "doc")
-      .withDomainEntityProperty("DomainEntity2", "doc")
-      .withBooleanProperty(duplicatePropertyName1, "because a property is required", true, false)
-      .withBooleanProperty(duplicatePropertyName2, "because a property is required", true, false)
+      .withDocumentation('because documentation is required')
+      .withDomainEntityProperty('DomainEntity1', 'doc')
+      .withDomainEntityProperty('DomainEntity2', 'doc')
+      .withBooleanProperty(duplicatePropertyName1, 'because a property is required', true, false)
+      .withBooleanProperty(duplicatePropertyName2, 'because a property is required', true, false)
       .withEndAssociation()
 
       .withStartAssociationExtension(entityName)
-      .withBooleanProperty(duplicatePropertyName1, "because a property is required", true, false)
-      .withBooleanProperty(duplicatePropertyName2, "because a property is required", true, false)
-      .withBooleanProperty(notDuplicatePropertyName, "because a property is required", true, false)
+      .withBooleanProperty(duplicatePropertyName1, 'because a property is required', true, false)
+      .withBooleanProperty(duplicatePropertyName2, 'because a property is required', true, false)
+      .withBooleanProperty(notDuplicatePropertyName, 'because a property is required', true, false)
       .withEndAssociationExtension()
-      .withEndNamespace().toString();
+      .withEndNamespace()
+      .toString();
       helper.setup(metaEdText, validatorListener, symbolTable);
     });
 
     it('should_have_validation_failure()', () => {
       helper.errorMessageCollection.length.should.equal(1);
     });
+
     it('should_have_validation_failure_message()', () => {
-      helper.errorMessageCollection[0].message.should.include("Association additions");
+      helper.errorMessageCollection[0].message.should.include('Association additions');
       helper.errorMessageCollection[0].message.should.include(entityName);
       helper.errorMessageCollection[0].message.should.include(duplicatePropertyName1);
       helper.errorMessageCollection[0].message.should.include(duplicatePropertyName2);
-      helper.errorMessageCollection[0].message.should.include("already in property list");
+      helper.errorMessageCollection[0].message.should.include('already in property list');
       helper.errorMessageCollection[0].message.should.not.include(notDuplicatePropertyName);
     });
   });
 
   describe('When_association_extension_has_duplicate_include_property', () => {
     const symbolTable = new SymbolTable();
-    const validatorListener = new ValidatorListener(
-      new TestRuleProvider(MetaEdGrammar.RULE_associationExtension, new AssociationExtensionMustNotDuplicateAssociationPropertyName(symbolTable)));
-
-    let entityName: string = "MyIdentifier";
-    const duplicatePropertyName: string = "Property1";
-    let helper: ValidatorTestHelper = new ValidatorTestHelper();
+    const entityName: string = 'MyIdentifier';
+    const duplicatePropertyName: string = 'Property1';
+    const helper: ValidatorTestHelper = new ValidatorTestHelper();
     before(() => {
-      let metaEdText = MetaEdTextBuilder.build()
-      .withBeginNamespace("edfi")
+      const metaEdText = MetaEdTextBuilder.build()
+      .withBeginNamespace('edfi')
       .withStartAssociation(entityName)
-      .withDocumentation("doc")
-      .withDomainEntityProperty("DomainEntity1", "doc")
-      .withDomainEntityProperty("DomainEntity2", "doc")
-      .withIncludeProperty(duplicatePropertyName, "doc", true, false)
+      .withDocumentation('doc')
+      .withDomainEntityProperty('DomainEntity1', 'doc')
+      .withDomainEntityProperty('DomainEntity2', 'doc')
+      .withIncludeProperty(duplicatePropertyName, 'doc', true, false)
       .withEndAssociation()
 
       .withStartAssociationExtension(entityName)
-      .withIncludeProperty(duplicatePropertyName, "doc", true, false)
+      .withIncludeProperty(duplicatePropertyName, 'doc', true, false)
       .withEndAssociationExtension()
-      .withEndNamespace().toString();
+      .withEndNamespace()
+      .toString();
       helper.setup(metaEdText, validatorListener, symbolTable);
     });
 
@@ -150,26 +146,24 @@ describe('AssociationExtensionMustNotDuplicateAssociationPropertyName', () => {
 
   describe('When_association_extension_has_duplicate_include_extension_override_property', () => {
     const symbolTable = new SymbolTable();
-    const validatorListener = new ValidatorListener(
-      new TestRuleProvider(MetaEdGrammar.RULE_associationExtension, new AssociationExtensionMustNotDuplicateAssociationPropertyName(symbolTable)));
-
-    let entityName: string = "MyIdentifier";
-    const duplicatePropertyName: string = "Property1";
-    let helper: ValidatorTestHelper = new ValidatorTestHelper();
+    const entityName: string = 'MyIdentifier';
+    const duplicatePropertyName: string = 'Property1';
+    const helper: ValidatorTestHelper = new ValidatorTestHelper();
     before(() => {
-      let metaEdText = MetaEdTextBuilder.build()
-      .withBeginNamespace("edfi")
+      const metaEdText = MetaEdTextBuilder.build()
+      .withBeginNamespace('edfi')
       .withStartAssociation(entityName)
-      .withDocumentation("doc")
-      .withDomainEntityProperty("DomainEntity1", "doc")
-      .withDomainEntityProperty("DomainEntity2", "doc")
-      .withIncludeProperty(duplicatePropertyName, "doc", true, false)
+      .withDocumentation('doc')
+      .withDomainEntityProperty('DomainEntity1', 'doc')
+      .withDomainEntityProperty('DomainEntity2', 'doc')
+      .withIncludeProperty(duplicatePropertyName, 'doc', true, false)
       .withEndAssociation()
 
       .withStartAssociationExtension(entityName)
-      .withIncludeExtensionOverrideProperty(duplicatePropertyName, "doc", true, false)
+      .withIncludeExtensionOverrideProperty(duplicatePropertyName, 'doc', true, false)
       .withEndAssociationExtension()
-      .withEndNamespace().toString();
+      .withEndNamespace()
+      .toString();
       helper.setup(metaEdText, validatorListener, symbolTable);
     });
 
