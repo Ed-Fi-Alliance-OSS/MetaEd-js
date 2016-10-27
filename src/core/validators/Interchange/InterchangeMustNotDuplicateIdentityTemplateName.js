98607bc929bca0643f56dab2a10a1b6b6f5b6105
Brad Banister <brad@doublelinepartners.com>
2016-10-27 14:27:44 -0500

    refactor interchange duplicates validators

--- src/core/validators/Interchange/InterchangeMustNotDuplicateIdentityTemplateName.js
+++ src/core/validators/Interchange/InterchangeMustNotDuplicateIdentityTemplateName.js
@@ -1,24 +1,20 @@
 // @flow
-import type SymbolTable from '../SymbolTable';
 import { interchangeErrorRule, includeInterchangeRule } from './InterchangeValidationRule';
-import { findDuplicates } from '../ValidationHelper';
+import { validForDuplicates, failureMessageForDuplicates } from '../ValidatorShared/InterchangeMustNotDuplicate';
 
 function idsToCheck(ruleContext: any) {
   return ruleContext.interchangeComponent().interchangeIdentityTemplate().map(x => x.ID().getText());
 }
 
-// eslint-disable-next-line no-unused-vars
-function valid(ruleContext: any, symbolTable: SymbolTable): boolean {
-  return findDuplicates(idsToCheck(ruleContext)).length === 0;
-}
+const valid = validForDuplicates(idsToCheck);
 
-// eslint-disable-next-line no-unused-vars
-function failureMessage(ruleContext: any, symbolTable: SymbolTable): string {
-  const identifier = ruleContext.interchangeName().getText();
-  const duplicates = findDuplicates(idsToCheck(ruleContext));
-  const joinString = '\', \'';
-  return `Interchange '${identifier}' declares duplicate identity template${duplicates.length > 1 ? 's' : ''} '${duplicates.join(joinString)}'.`;
-}
+const failureMessage =
+  failureMessageForDuplicates(
+    'Interchange',
+    'identity template',
+    (ruleContext: any): string => ruleContext.interchangeName().getText(),
+    idsToCheck
+  );
 
 const validationRule = interchangeErrorRule(valid, failureMessage);
 export { validationRule as default };
