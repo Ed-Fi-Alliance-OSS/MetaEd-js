0b982e3ca98894424b9abe0695214bba2db362aa
Brad Banister <brad@doublelinepartners.com>
2016-12-06 16:08:42 -0600

    switch to yargs, better validation

--- src/console/metaed-console.js
+++ src/console/metaed-console.js
@@ -1,32 +1,39 @@
 // @flow
-import commander from 'commander';
 import winston from 'winston';
 import start from '../core/tasks/Pipeline';
 import { StateInstance } from '../core/State';
 // eslint-disable-next-line no-duplicate-imports
 import type { State } from '../core/State';
 
-commander.version('0.8')
-.option('--edfi <coreDir>', 'The base path where core MetaEd files will be loaded from. The directory must currently exist.')
-.option('--ext <extensionDir>', 'The base path where extension MetaEd files will be loaded from. If provided, the directory must currently exist.')
-.parse(process.argv);
+const argv = require('yargs')
+  .usage('Usage: $0 [options]')
+  .alias('e', 'edfi')
+  .nargs('e', 1)
+  .describe('e', 'The base path where core MetaEd files will be loaded from. The directory must currently exist.')
+  .alias('x', 'ext')
+  .nargs('x', 1)
+  .describe('x', 'The base path where extension MetaEd files will be loaded from. If provided, the directory must currently exist.')
+  .demand(['e', 'x'])
+  .help('h')
+  .alias('h', 'help')
+  .argv;
 
 winston.level = 'info';
 
-winston.info(`Executing MetaEd Console on core ${commander.edfi} and extension ${commander.ext}.`);
+winston.info(`Executing MetaEd Console on core ${argv.edfi} and extension ${argv.ext}.`);
 winston.info('');
 
 // $FlowFixMe -- doesn't like constructor call on Immutable.Record
 const state: State = new StateInstance({
   inputDirectories: [
     {
-      path: commander.edfi,
+      path: argv.edfi,
       namespace: 'edfi',
       projectExtension: '',
       isExtension: false,
     },
     {
-      path: commander.ext,
+      path: argv.ext,
       namespace: 'extension',
       projectExtension: 'EXTENSION',
       isExtension: true,
