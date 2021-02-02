f3be00c9bd8c5fcd573a8a5ac4a0eb76a319984a
Stephen A. Fuqua <stephen@safnet.com>
2021-02-02 14:28:43 -0600

    remove debug statements

--- .github/actions/npmrc/index.js
+++ .github/actions/npmrc/index.js
@@ -3,18 +3,13 @@ const fs = require('fs');
 
 async function run() {
   try {
-    console.log('111111111111111111111');
     const feedUrl = core.getInput('feed-url');
-    console.log('2222222222222222222222');
     const personalAccessToken = core.getInput('personal-access-token');
-    console.log('3333333333333333333333');
     const isWindows = core.getInput('is-windows');
-    console.log('4444444444444444444444444');
 
     fileContents = `registry=https://www.myget.org/F/ed-fi/npm/
     //www.myget.org/F/ed-fi/npm/:_authToken=${personalAccessToken}
 `;
-    console.log('5555555555555555555555555');
 
     var filePath = `${process.env.HOME}/.npmrc`;
 
@@ -22,9 +17,8 @@ async function run() {
       `${process.env.USERPROFILE}\\.npmrc`;
     }
 
-    console.log('666666666666666666666666666666666');
     fs.writeFile(filePath, fileContents, err => { if(err) throw err; });
-    console.log('77777777777777777777777777');
+
   } catch (error) {
     setFailed(error.message);
   }
