2dfccf3fb790b52386dab7117a513e04e355822e
Stephen A. Fuqua <stephen@safnet.com>
2021-10-07 17:14:49 -0500

    This does not work, sharing for discussion only

--- packages/metaed-plugin-edfi-unified-advanced/src/validator/CommonProperty/CommonPropertyCollectionTargetMustContainIdentity.ts
+++ packages/metaed-plugin-edfi-unified-advanced/src/validator/CommonProperty/CommonPropertyCollectionTargetMustContainIdentity.ts
@@ -1,11 +1,20 @@
-import { MetaEdEnvironment, ValidationFailure } from 'metaed-core';
+import { MetaEdEnvironment, ValidationFailure, CommonProperty } from 'metaed-core';
+
+function hasIdentity(commonProperty: CommonProperty): Boolean {
+  return (
+    commonProperty.referencedEntity.identityProperties.length > 0 ||
+    (commonProperty.referencedEntity.baseEntity !== null &&
+      commonProperty.referencedEntity.baseEntity!.properties.some(property => property.isPartOfIdentity))
+  );
+}
 
 export function validate(metaEd: MetaEdEnvironment): ValidationFailure[] {
   const failures: ValidationFailure[] = [];
 
   metaEd.propertyIndex.common.forEach(commonProperty => {
     if (!commonProperty.isOptionalCollection && !commonProperty.isRequiredCollection) return;
-    if (!commonProperty.referencedEntity.properties.some(property => property.isPartOfIdentity)) {
+
+    if (!hasIdentity(commonProperty)) {
       failures.push({
         validatorName: 'CommonPropertyCollectionTargetMustContainIdentity',
         category: 'error',
