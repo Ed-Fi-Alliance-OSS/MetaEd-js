2a294e495832db87e95ee51492204d60a5be1753
Stephen A. Fuqua <stephen@safnet.com>
2022-02-07 10:55:19 -0600

    Add a pause

--- build/nvm-select-version.ps1
+++ build/nvm-select-version.ps1
@@ -8,4 +8,8 @@ param (
 nvm version
 nvm install $version
 nvm use $version
+
+# wait a few seconds before running the following, making it less likely to have a misleading failure
+Start-Sleep -Seconds 5
+
 npm version
