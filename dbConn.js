873c08bba5a29f5ba95a62ef3186ed30b2579a0e
Steven Arnold <steven.arnold@ed-fi.org>
2022-04-21 15:48:29 -0500

    Create dbConn.js

---
+++ dbConn.js
@@ -0,0 +1,21 @@
+const sql = require('mssql');
+
+const config = {
+    user: 'sa',
+    password: 'abcdefghi1!',
+    server: 'localhost',
+    database: 'master',
+};
+
+sql.on('error', err => {
+    console.error('err: ', err);
+});
+
+sql.connect(config).then(pool => {
+    return pool.request()
+        .query('select @@VERSION')
+}).then(result => {
+    console.dir(result)
+}).catch(err => {
+    console.error('err: ', err);
+});
