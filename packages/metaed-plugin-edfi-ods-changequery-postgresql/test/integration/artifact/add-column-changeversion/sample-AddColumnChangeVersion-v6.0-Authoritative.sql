8919863da2a00a9c91499a417521f319b0da3e87
Brad Banister <brad@banisterconsult.com>
2022-07-18 19:27:03 -0500

    [METAED-1281] postgresql version

--- packages/metaed-plugin-edfi-ods-changequery-postgresql/test/integration/artifact/add-column-changeversion/sample-AddColumnChangeVersion-v6.0-Authoritative.sql
+++ packages/metaed-plugin-edfi-ods-changequery-postgresql/test/integration/artifact/add-column-changeversion/sample-AddColumnChangeVersion-v6.0-Authoritative.sql
@@ -1,7 +1,8 @@
 DO $$
 BEGIN
 IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='sample' AND table_name='PostSecondaryOrganization' AND column_name='changeversion') THEN
-ALTER TABLE sample.PostSecondaryOrganization ADD ChangeVersion BIGINT DEFAULT nextval('changes.ChangeVersionSequence') NOT NULL;
+ALTER TABLE sample.PostSecondaryOrganization ADD ChangeVersion BIGINT DEFAULT (0) NOT NULL; 
+ALTER TABLE sample.PostSecondaryOrganization ALTER ChangeVersion SET DEFAULT nextval('changes.ChangeVersionSequence');
 END IF;
 
 END
