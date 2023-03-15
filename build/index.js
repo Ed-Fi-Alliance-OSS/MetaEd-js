e064b8886f700648d69e8bd91fa405e069a4f324
Stephen A. Fuqua <stephen@safnet.com>
2023-03-15 13:40:22 -0500

    Simplify the script

--- build/index.js
+++ build/index.js
@@ -1,16 +1,5 @@
 /* eslint-disable no-console */
-
-import { execSync } from 'child_process';
-
 console.info('Command arguments: ', process.argv);
-
-try {
-  execSync('npm install @edfi/metaed-console@dev --loglevel error');
-} catch (err) {
-  console.error('Exception occurred during npm install atom-metaed:');
-  console.error('--> stdout', err);
-  console.error('--> sdterr', err.stderr.toString());
-}
 
 // Implicitly calls the deploy process
 // eslint-disable-next-line import/no-extraneous-dependencies
