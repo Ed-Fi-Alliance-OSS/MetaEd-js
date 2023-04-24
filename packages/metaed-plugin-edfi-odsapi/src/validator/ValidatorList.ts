ab76518bcd612e2376a9ed1e090f823e5b88fea4
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2023-04-24 12:03:59 -0600

    Removes UpcomingImprovements/ExtendingSubclassOfEducationOrganizationProhibited and UpcomingImprovements/SubclassingAnyAssociationExceptStudentProgramAssociationIsUnsupportedV2 and its tests.

--- packages/metaed-plugin-edfi-odsapi/src/validator/ValidatorList.ts
+++ packages/metaed-plugin-edfi-odsapi/src/validator/ValidatorList.ts
@@ -1,18 +1,14 @@
 import { Validator } from '@edfi/metaed-core';
 
-import { validate as extendingSubclassOfEducationOrganizationProhibited } from './UpcomingImprovements/ExtendingSubclassOfEducationOrganizationProhibited';
 import { validate as includingRequiredPropertiesInExtendedAssociationsProhibited } from './UpcomingImprovements/IncludingRequiredPropertiesInExtendedAssociationsProhibited';
 import { validate as includingRequiredPropertiesInExtendedDomainEntitiesProhibited } from './UpcomingImprovements/IncludingRequiredPropertiesInExtendedDomainEntitiesProhibited';
-import { validate as subclassingAnyAssociationExceptStudentProgramAssociationIsUnsupportedV2 } from './UpcomingImprovements/SubclassingAnyAssociationExceptStudentProgramAssociationIsUnsupportedV2';
 import { validate as subclassingAnyAssociationExceptGeneralStudentProgramAssociationIsUnsupported } from './UpcomingImprovements/SubclassingAnyAssociationExceptGeneralStudentProgramAssociationIsUnsupported';
 import { validate as subclassingAnyDomainEntityExceptEducationOrganizationIsUnsupported } from './UpcomingImprovements/SubclassingAnyDomainEntityExceptEducationOrganizationIsUnsupported';
 
 export function validatorList(): Validator[] {
   return [
-    extendingSubclassOfEducationOrganizationProhibited,
     includingRequiredPropertiesInExtendedAssociationsProhibited,
     includingRequiredPropertiesInExtendedDomainEntitiesProhibited,
-    subclassingAnyAssociationExceptStudentProgramAssociationIsUnsupportedV2,
     subclassingAnyAssociationExceptGeneralStudentProgramAssociationIsUnsupported,
     subclassingAnyDomainEntityExceptEducationOrganizationIsUnsupported,
   ];
