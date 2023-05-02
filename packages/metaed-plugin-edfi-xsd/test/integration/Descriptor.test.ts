f107c5b2be005c8fde3edc895fdb26bbafa3a7a0
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2023-05-02 14:54:48 -0600

    Restoring tests previously deleted. Fix unit tests.

--- packages/metaed-plugin-edfi-xsd/test/integration/Descriptor.test.ts
+++ packages/metaed-plugin-edfi-xsd/test/integration/Descriptor.test.ts
@@ -40,6 +40,11 @@ describe('when generating xsd for descriptor', (): void => {
     const elements = xpathSelect("/xs:schema/xs:complexType[@name='CoreEntityDescriptor']", coreResult);
     expect(elements).toHaveLength(1);
   });
+
+  it('should generate descriptor reference', (): void => {
+    const elements = xpathSelect("/xs:schema/xs:simpleType[@name='CoreEntityDescriptorReferenceType']", coreResult);
+    expect(elements).toHaveLength(1);
+  });
 });
 
 describe('when generating xsd for domain entity in extension namespace with reference to core descriptor', (): void => {
@@ -89,6 +94,11 @@ describe('when generating xsd for domain entity in extension namespace with refe
 
   it('should generate core descriptor', (): void => {
     const elements = xpathSelect("/xs:schema/xs:complexType[@name='CoreEntityDescriptor']", coreResult);
+    expect(elements).toHaveLength(1);
+  });
+
+  it('should generate core domain entity reference', (): void => {
+    const elements = xpathSelect("/xs:schema/xs:simpleType[@name='CoreEntityDescriptorReferenceType']", coreResult);
     expect(elements).toHaveLength(1);
   });
 
