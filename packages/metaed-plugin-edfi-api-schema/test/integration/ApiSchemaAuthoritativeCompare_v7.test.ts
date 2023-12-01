652aae7ff6f7a5af6aac5be98c793e2977b270df
Brad Banister <brad@banisterconsult.com>
2023-12-01 14:38:57 -0600

    [METAED-1494] final fixup

--- packages/metaed-plugin-edfi-api-schema/test/integration/ApiSchemaAuthoritativeCompare_v7.test.ts
+++ packages/metaed-plugin-edfi-api-schema/test/integration/ApiSchemaAuthoritativeCompare_v7.test.ts
@@ -22,7 +22,7 @@ jest.setTimeout(40000);
 
 // Skipping because there is an ordering problem in the JSON file between running locally on Ubuntu and on GitHub actions
 describe.skip('when generating ods and comparing it to data standard 5.0 authoritative artifacts for ODS/API 7.0', (): void => {
-  const artifactPath: string = path.resolve(__dirname, './artifact/v7/');
+  const artifactPath: string = path.resolve(__dirname, './artifact/v7_1/');
   const authoritativeCoreFilename = 'ds-5.0-api-schema-authoritative.json';
   const generatedCoreFilename = 'ds-5.0-api-schema-generated.json';
 
