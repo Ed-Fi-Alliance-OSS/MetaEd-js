6240403aa33da072659a59edf7d08001d27d457b
Adam Hopkins <adam.hopkins@simpat.tech>
2024-06-19 12:42:36 -0500

    BooleanNumericJsonPathsEnhancer

---
+++ packages/metaed-plugin-edfi-api-schema/src/enhancer/BooleanNumericJsonPathsEnhancer.ts
@@ -0,0 +1,57 @@
+import { getAllEntitiesOfType, MetaEdEnvironment, EnhancerResult } from '@edfi/metaed-core';
+import { EntityApiSchemaData } from '../model/EntityApiSchemaData';
+import { JsonPath } from '../model/api-schema/JsonPath';
+
+/**
+ * Accumulates the identityJsonPaths for the boolean and numeric properties of an entity
+ */
+export function enhance(metaEd: MetaEdEnvironment): EnhancerResult {
+  getAllEntitiesOfType(metaEd, 'domainEntity', 'association', 'domainEntitySubclass', 'associationSubclass').forEach(
+    (entity) => {
+      // Using Set to remove duplicates
+      const booleanResult: Set<JsonPath> = new Set();
+      const numericResult: Set<JsonPath> = new Set();
+      const numericTypes = [
+        'currency',
+        'decimal',
+        'duration',
+        'integer',
+        'percent',
+        'schoolYearEnumeration',
+        'sharedDecimal',
+        'sharedInteger',
+        'sharedShort',
+        'sharedString',
+        'short',
+        'year',
+      ];
+
+      const { allJsonPathsMapping } = entity.data.edfiApiSchema as EntityApiSchemaData;
+
+      Object.entries(allJsonPathsMapping).forEach(([, jsonPathsInfo]) => {
+        jsonPathsInfo.jsonPathPropertyPairs.forEach((jppp) => {
+          if (jppp.sourceProperty.type === 'boolean') {
+            booleanResult.add(jppp.jsonPath);
+          } else if (numericTypes.indexOf(jppp.sourceProperty.type) >= 0) {
+            numericResult.add(jppp.jsonPath);
+          }
+        });
+      });
+
+      (entity.data.edfiApiSchema as EntityApiSchemaData).booleanJsonPaths = [...booleanResult].sort();
+      (entity.data.edfiApiSchema as EntityApiSchemaData).numericJsonPaths = [...numericResult].sort();
+    },
+  );
+
+  // Descriptors have no boolean or numeric properties
+  getAllEntitiesOfType(metaEd, 'descriptor').forEach((entity) => {
+    const edfiApiSchemaData = entity.data.edfiApiSchema as EntityApiSchemaData;
+    edfiApiSchemaData.booleanJsonPaths = [];
+    edfiApiSchemaData.numericJsonPaths = [];
+  });
+
+  return {
+    enhancerName: 'BooleanNumericJsonPathsEnhancer',
+    success: true,
+  };
+}
