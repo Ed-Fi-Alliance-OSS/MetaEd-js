845ba483f0077105fa3051c27c70c4380ee14efd
Adam Hopkins <adam.hopkins@simpat.tech>
2024-06-20 08:56:55 -0500

    Add enhanced test scenario

--- packages/metaed-plugin-edfi-api-schema/test/enhancer/BooleanNumericJsonPathsEnhancer.test.ts
+++ packages/metaed-plugin-edfi-api-schema/test/enhancer/BooleanNumericJsonPathsEnhancer.test.ts
@@ -11,9 +11,21 @@ import {
 import { domainEntityReferenceEnhancer } from '@edfi/metaed-plugin-edfi-unified';
 import { enhance as entityPropertyApiSchemaDataSetupEnhancer } from '../../src/model/EntityPropertyApiSchemaData';
 import { enhance as entityApiSchemaDataSetupEnhancer } from '../../src/model/EntityApiSchemaData';
+import { enhance as pluginEnvironmentSetupEnhancer } from '../../src/model/PluginEnvironment';
+import { enhance as subclassPropertyNamingCollisionEnhancer } from '../../src/enhancer/SubclassPropertyNamingCollisionEnhancer';
+import { enhance as referenceComponentEnhancer } from '../../src/enhancer/ReferenceComponentEnhancer';
 import { enhance as apiPropertyMappingEnhancer } from '../../src/enhancer/ApiPropertyMappingEnhancer';
+import { enhance as apiEntityMappingEnhancer } from '../../src/enhancer/ApiEntityMappingEnhancer';
+import { enhance as subclassApiEntityMappingEnhancer } from '../../src/enhancer/SubclassApiEntityMappingEnhancer';
 import { enhance as propertyCollectingEnhancer } from '../../src/enhancer/PropertyCollectingEnhancer';
+import { enhance as subclassPropertyCollectingEnhancer } from '../../src/enhancer/SubclassPropertyCollectingEnhancer';
+import { enhance as jsonSchemaEnhancerForInsert } from '../../src/enhancer/JsonSchemaEnhancerForInsert';
 import { enhance as allJsonPathsMappingEnhancer } from '../../src/enhancer/AllJsonPathsMappingEnhancer';
+import { enhance as mergeDirectiveEqualityConstraintEnhancer } from '../../src/enhancer/MergeDirectiveEqualityConstraintEnhancer';
+import { enhance as resourceNameEnhancer } from '../../src/enhancer/ResourceNameEnhancer';
+import { enhance as identityFullnameEnhancer } from '../../src/enhancer/IdentityFullnameEnhancer';
+import { enhance as subclassIdentityFullnameEnhancer } from '../../src/enhancer/SubclassIdentityFullnameEnhancer';
+import { enhance as documentPathsMappingEnhancer } from '../../src/enhancer/DocumentPathsMappingEnhancer';
 import { enhance as booleanNumericJsonPathsEnhancer } from '../../src/enhancer/BooleanNumericJsonPathsEnhancer';
 import { enhance } from '../../src/enhancer/IdentityJsonPathsEnhancer';
 
@@ -23,9 +35,21 @@ addFormatsTo(ajv);
 function runApiSchemaEnhancers(metaEd: MetaEdEnvironment) {
   entityPropertyApiSchemaDataSetupEnhancer(metaEd);
   entityApiSchemaDataSetupEnhancer(metaEd);
+  pluginEnvironmentSetupEnhancer(metaEd);
+  subclassPropertyNamingCollisionEnhancer(metaEd);
+  referenceComponentEnhancer(metaEd);
   apiPropertyMappingEnhancer(metaEd);
   propertyCollectingEnhancer(metaEd);
+  subclassPropertyCollectingEnhancer(metaEd);
+  apiEntityMappingEnhancer(metaEd);
+  subclassApiEntityMappingEnhancer(metaEd);
+  jsonSchemaEnhancerForInsert(metaEd);
   allJsonPathsMappingEnhancer(metaEd);
+  mergeDirectiveEqualityConstraintEnhancer(metaEd);
+  resourceNameEnhancer(metaEd);
+  identityFullnameEnhancer(metaEd);
+  subclassIdentityFullnameEnhancer(metaEd);
+  documentPathsMappingEnhancer(metaEd);
   booleanNumericJsonPathsEnhancer(metaEd);
   enhance(metaEd);
 }
@@ -93,6 +117,50 @@ describe('when building simple domain entity with all the simple non-collections
           "$.requiredDurationProperty",
           "$.requiredIntegerProperty",
           "$.schoolYearTypeReference.schoolYear",
+        ]
+      `);
+  });
+});
+
+describe('when building domain entity with collections', () => {
+  const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
+  metaEd.plugin.set('edfiApiSchema', newPluginEnvironment());
+  const namespaceName = 'EdFi';
+
+  beforeAll(() => {
+    MetaEdTextBuilder.build()
+      .withBeginNamespace(namespaceName)
+      .withStartDomainEntity('AssessmentScoreRangeLearningStandard')
+      .withDocumentation('doc')
+      .withIntegerIdentity('ScoreRangeId', 'doc')
+      .withDomainEntityProperty('LearningStandard', 'doc', true, true)
+      .withEndDomainEntity()
+
+      .withStartDomainEntity('LearningStandard')
+      .withDocumentation('doc')
+      .withIntegerIdentity('LearningStandardId', 'doc')
+      .withEndDomainEntity()
+
+      .withEndNamespace()
+
+      .sendToListener(new NamespaceBuilder(metaEd, []))
+      .sendToListener(new DomainEntityBuilder(metaEd, []));
+
+    domainEntityReferenceEnhancer(metaEd);
+    runApiSchemaEnhancers(metaEd);
+  });
+
+  it('should be correct booleanJsonPaths and numericJsonPaths for AssessmentScoreRangeLearningStandard', () => {
+    const entity = metaEd.namespace.get(namespaceName)?.entity.domainEntity.get('AssessmentScoreRangeLearningStandard');
+    const booleanJsonPaths = entity?.data.edfiApiSchema.booleanJsonPaths;
+    const numericJsonPaths = entity?.data.edfiApiSchema.numericJsonPaths;
+
+    expect(booleanJsonPaths).toMatchInlineSnapshot(`Array []`);
+
+    expect(numericJsonPaths).toMatchInlineSnapshot(`
+        Array [
+          "$.learningStandards[*].learningStandardReference.learningStandardId",
+          "$.scoreRangeId",
         ]
       `);
   });
