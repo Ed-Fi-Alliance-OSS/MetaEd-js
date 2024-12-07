4e9e586ce56bc1bf701cab329eb7a5ac69dd19c1
Adam Hopkins <adam.hopkins@simpat.tech>
2024-12-07 08:28:30 -0600

    More static content for POST

--- packages/metaed-plugin-edfi-api-schema/src/enhancer/OpenApiSpecificationEnhancer.ts
+++ packages/metaed-plugin-edfi-api-schema/src/enhancer/OpenApiSpecificationEnhancer.ts
@@ -1,23 +1,84 @@
 import { MetaEdEnvironment, EnhancerResult, Namespace } from '@edfi/metaed-core';
 import { OpenAPIV3 } from 'openapi-types';
+import { NamespaceEdfiApiSchema } from '../model/Namespace';
+import { ProjectNamespace } from '../model/api-schema/ProjectNamespace';
 
 export function enhance(metaEd: MetaEdEnvironment): EnhancerResult {
-  const swaggerDocument: OpenAPIV3.Document = {
-    openapi: '3.0.0',
-    info: {
-      title: 'Ed-Fi Alliance Data Management Service',
-      description: 'Ed-Fi Alliance Data Management Service',
-      version: '0',
-    },
-    servers: [
-      {
-        url: 'http://localhost:5198/',
-      },
-    ],
-    paths: {},
-  };
-
   metaEd.namespace.forEach((namespace: Namespace) => {
+    const { apiSchema } = namespace.data.edfiApiSchema as NamespaceEdfiApiSchema;
+    const projectNamespace: ProjectNamespace = namespace.projectName.toLowerCase() as ProjectNamespace;
+
+    const paths: OpenAPIV3.PathsObject = Object.fromEntries(
+      Object.entries(apiSchema.projectSchemas[projectNamespace].caseInsensitiveEndpointNameMapping).map(([k]) => [
+        `/${projectNamespace}/${k}`,
+        {
+          post: {
+            description:
+              'The POST operation can be used to create or update resources. In database terms, this is often referred to as an "upsert" operation (insert + update). Clients should NOT include the resource "id" in the JSON body because it will result in an error. The web service will identify whether the resource already exists based on the natural key values provided, and update or create the resource appropriately. It is recommended to use POST for both create and update except while updating natural key of a resource in which case PUT operation must be used.',
+            operationId: `post-${k}`,
+            requestBody: {
+              description: `The JSON representation of the ${k} resource to be created or updated.`,
+              content: {
+                'application/json': {
+                  schema: {
+                    $ref: `#/components/schemas/edFi_${k}`,
+                  },
+                },
+              },
+              required: true,
+              'x-bodyName': k,
+            },
+            responses: {
+              200: {
+                $ref: '#/components/responses/Updated',
+              },
+              201: {
+                $ref: '#/components/responses/Created',
+              },
+              400: {
+                $ref: '#/components/responses/BadRequest',
+              },
+              401: {
+                $ref: '#/components/responses/Unauthorized',
+              },
+              403: {
+                $ref: '#/components/responses/Forbidden',
+              },
+              405: {
+                description:
+                  'Method Is Not Allowed. When the Use-Snapshot header is set to true, the method is not allowed.',
+              },
+              409: {
+                $ref: '#/components/responses/Conflict',
+              },
+              412: {
+                $ref: '#/components/responses/PreconditionFailed',
+              },
+              500: {
+                $ref: '#/components/responses/Error',
+              },
+            },
+            summary: 'Creates or updates resources based on the natural key values of the supplied resource.',
+            tags: [`${k}`],
+          },
+        },
+      ]),
+    );
+
+    const swaggerDocument: OpenAPIV3.Document = {
+      openapi: '3.0.0',
+      info: {
+        title: 'Ed-Fi Alliance Data Management Service',
+        description: 'Ed-Fi Alliance Data Management Service',
+        version: '0',
+      },
+      servers: [
+        {
+          url: 'http://localhost:5198/',
+        },
+      ],
+      paths,
+    };
     namespace.data.openApiSpecification = swaggerDocument;
   });
 
--- packages/metaed-plugin-edfi-api-schema/test/enhancer/OpenApiSpecificationEnhancer.test.ts
+++ packages/metaed-plugin-edfi-api-schema/test/enhancer/OpenApiSpecificationEnhancer.test.ts
@@ -1,21 +1,95 @@
-import { MetaEdEnvironment, MetaEdTextBuilder, NamespaceBuilder, newMetaEdEnvironment } from '@edfi/metaed-core';
+import {
+  DomainEntityBuilder,
+  MetaEdEnvironment,
+  MetaEdTextBuilder,
+  NamespaceBuilder,
+  newMetaEdEnvironment,
+} from '@edfi/metaed-core';
+import { domainEntityReferenceEnhancer } from '@edfi/metaed-plugin-edfi-unified';
+import { enhance as entityPropertyApiSchemaDataSetupEnhancer } from '../../src/model/EntityPropertyApiSchemaData';
+import { enhance as entityApiSchemaDataSetupEnhancer } from '../../src/model/EntityApiSchemaData';
+import { enhance as namespaceSetupEnhancer } from '../../src/model/Namespace';
+import { enhance as subclassPropertyNamingCollisionEnhancer } from '../../src/enhancer/SubclassPropertyNamingCollisionEnhancer';
+import { enhance as referenceComponentEnhancer } from '../../src/enhancer/ReferenceComponentEnhancer';
+import { enhance as apiPropertyMappingEnhancer } from '../../src/enhancer/ApiPropertyMappingEnhancer';
+import { enhance as apiEntityMappingEnhancer } from '../../src/enhancer/ApiEntityMappingEnhancer';
+import { enhance as subclassApiEntityMappingEnhancer } from '../../src/enhancer/SubclassApiEntityMappingEnhancer';
+import { enhance as propertyCollectingEnhancer } from '../../src/enhancer/PropertyCollectingEnhancer';
+import { enhance as subclassPropertyCollectingEnhancer } from '../../src/enhancer/SubclassPropertyCollectingEnhancer';
+import { enhance as jsonSchemaEnhancerForInsert } from '../../src/enhancer/JsonSchemaEnhancerForInsert';
+import { enhance as allJsonPathsMappingEnhancer } from '../../src/enhancer/AllJsonPathsMappingEnhancer';
+import { enhance as mergeDirectiveEqualityConstraintEnhancer } from '../../src/enhancer/MergeDirectiveEqualityConstraintEnhancer';
+import { enhance as resourceNameEnhancer } from '../../src/enhancer/ResourceNameEnhancer';
+import { enhance as identityFullnameEnhancer } from '../../src/enhancer/IdentityFullnameEnhancer';
+import { enhance as subclassIdentityFullnameEnhancer } from '../../src/enhancer/SubclassIdentityFullnameEnhancer';
+import { enhance as identityJsonPathsEnhancer } from '../../src/enhancer/IdentityJsonPathsEnhancer';
+import { enhance as documentPathsMappingEnhancer } from '../../src/enhancer/DocumentPathsMappingEnhancer';
+import { enhance as typeCoercionJsonPathsEnhancer } from '../../src/enhancer/TypeCoercionJsonPathsEnhancer';
+import { enhance as apiSchemaBuildingEnhancer } from '../../src/enhancer/ApiSchemaBuildingEnhancer';
 import { enhance } from '../../src/enhancer/OpenApiSpecificationEnhancer';
+
+function runApiSchemaEnhancers(metaEd: MetaEdEnvironment) {
+  namespaceSetupEnhancer(metaEd);
+  entityPropertyApiSchemaDataSetupEnhancer(metaEd);
+  entityApiSchemaDataSetupEnhancer(metaEd);
+  subclassPropertyNamingCollisionEnhancer(metaEd);
+  referenceComponentEnhancer(metaEd);
+  apiPropertyMappingEnhancer(metaEd);
+  propertyCollectingEnhancer(metaEd);
+  subclassPropertyCollectingEnhancer(metaEd);
+  apiEntityMappingEnhancer(metaEd);
+  subclassApiEntityMappingEnhancer(metaEd);
+  jsonSchemaEnhancerForInsert(metaEd);
+  allJsonPathsMappingEnhancer(metaEd);
+  mergeDirectiveEqualityConstraintEnhancer(metaEd);
+  resourceNameEnhancer(metaEd);
+  identityFullnameEnhancer(metaEd);
+  subclassIdentityFullnameEnhancer(metaEd);
+  identityJsonPathsEnhancer(metaEd);
+  documentPathsMappingEnhancer(metaEd);
+  typeCoercionJsonPathsEnhancer(metaEd);
+  apiSchemaBuildingEnhancer(metaEd);
+  enhance(metaEd);
+}
 
 describe('when building open api specification', () => {
   const metaEd: MetaEdEnvironment = newMetaEdEnvironment();
   const namespaceName = 'EdFi';
+  const domainEntityName = 'DomainEntityName';
   let namespace: any = null;
 
   beforeAll(() => {
-    MetaEdTextBuilder.build().withBeginNamespace(namespaceName).sendToListener(new NamespaceBuilder(metaEd, []));
+    MetaEdTextBuilder.build()
+      .withBeginNamespace(namespaceName)
+      .withStartDomainEntity(domainEntityName)
+      .withDocumentation('doc')
+      .withBooleanProperty('OptionalBooleanProperty', 'doc1', false, false)
+      .withCurrencyProperty('RequiredCurrencyProperty', 'doc2', true, false)
+      .withDecimalProperty('OptionalDecimalProperty', 'doc3', false, false, '2', '1')
+      .withDurationProperty('RequiredDurationProperty', 'doc4', true, false)
+      .withPercentProperty('OptionalPercentProperty', 'doc5', false, false)
+      .withDateProperty('RequiredDateProperty', 'doc6', true, false)
+      .withDatetimeProperty('RequiredDatetimeProperty', 'doc7', true, false)
+      .withIntegerProperty('RequiredIntegerProperty', 'doc8', true, false, '10', '5')
+      .withShortProperty('OptionalShortProperty', 'doc9', false, false)
+      .withStringIdentity('StringIdentity', 'doc10', '30', '20')
+      .withTimeProperty('RequiredTimeProperty', 'doc11', true, false)
+      .withEnumerationProperty('SchoolYear', 'doc12', false, false)
+      .withYearProperty('OptionalYear', 'doc13', false, false)
+      .withEndDomainEntity()
+      .withEndNamespace()
+      .sendToListener(new NamespaceBuilder(metaEd, []))
+      .sendToListener(new DomainEntityBuilder(metaEd, []));
+
+    domainEntityReferenceEnhancer(metaEd);
+    runApiSchemaEnhancers(metaEd);
 
     namespace = metaEd.namespace.get(namespaceName);
-
-    enhance(metaEd);
   });
 
   it('should be correct OpenApiSpecification', () => {
     const openApiSpec = namespace.data.openApiSpecification;
+
     expect(openApiSpec).toMatchInlineSnapshot(`
       Object {
         "info": Object {
@@ -23,7 +97,111 @@ describe('when building open api specification', () => {
           "title": "Ed-Fi Alliance Data Management Service",
           "version": "0",
         },
-        "openapi": "3.0.1",
+        "openapi": "3.0.0",
+        "paths": Object {
+          "/edfi/domainentitynames": Object {
+            "post": Object {
+              "description": "The POST operation can be used to create or update resources. In database terms, this is often referred to as an \\"upsert\\" operation (insert + update). Clients should NOT include the resource \\"id\\" in the JSON body because it will result in an error. The web service will identify whether the resource already exists based on the natural key values provided, and update or create the resource appropriately. It is recommended to use POST for both create and update except while updating natural key of a resource in which case PUT operation must be used.",
+              "operationId": "post-domainentitynames",
+              "requestBody": Object {
+                "content": Object {
+                  "application/json": Object {
+                    "schema": Object {
+                      "$ref": "#/components/schemas/edFi_domainentitynames",
+                    },
+                  },
+                },
+                "description": "The JSON representation of the domainentitynames resource to be created or updated.",
+                "required": true,
+                "x-bodyName": "domainentitynames",
+              },
+              "responses": Object {
+                "200": Object {
+                  "$ref": "#/components/responses/Updated",
+                },
+                "201": Object {
+                  "$ref": "#/components/responses/Created",
+                },
+                "400": Object {
+                  "$ref": "#/components/responses/BadRequest",
+                },
+                "401": Object {
+                  "$ref": "#/components/responses/Unauthorized",
+                },
+                "403": Object {
+                  "$ref": "#/components/responses/Forbidden",
+                },
+                "405": Object {
+                  "description": "Method Is Not Allowed. When the Use-Snapshot header is set to true, the method is not allowed.",
+                },
+                "409": Object {
+                  "$ref": "#/components/responses/Conflict",
+                },
+                "412": Object {
+                  "$ref": "#/components/responses/PreconditionFailed",
+                },
+                "500": Object {
+                  "$ref": "#/components/responses/Error",
+                },
+              },
+              "summary": "Creates or updates resources based on the natural key values of the supplied resource.",
+              "tags": Array [
+                "domainentitynames",
+              ],
+            },
+          },
+          "/edfi/schoolyeartypes": Object {
+            "post": Object {
+              "description": "The POST operation can be used to create or update resources. In database terms, this is often referred to as an \\"upsert\\" operation (insert + update). Clients should NOT include the resource \\"id\\" in the JSON body because it will result in an error. The web service will identify whether the resource already exists based on the natural key values provided, and update or create the resource appropriately. It is recommended to use POST for both create and update except while updating natural key of a resource in which case PUT operation must be used.",
+              "operationId": "post-schoolyeartypes",
+              "requestBody": Object {
+                "content": Object {
+                  "application/json": Object {
+                    "schema": Object {
+                      "$ref": "#/components/schemas/edFi_schoolyeartypes",
+                    },
+                  },
+                },
+                "description": "The JSON representation of the schoolyeartypes resource to be created or updated.",
+                "required": true,
+                "x-bodyName": "schoolyeartypes",
+              },
+              "responses": Object {
+                "200": Object {
+                  "$ref": "#/components/responses/Updated",
+                },
+                "201": Object {
+                  "$ref": "#/components/responses/Created",
+                },
+                "400": Object {
+                  "$ref": "#/components/responses/BadRequest",
+                },
+                "401": Object {
+                  "$ref": "#/components/responses/Unauthorized",
+                },
+                "403": Object {
+                  "$ref": "#/components/responses/Forbidden",
+                },
+                "405": Object {
+                  "description": "Method Is Not Allowed. When the Use-Snapshot header is set to true, the method is not allowed.",
+                },
+                "409": Object {
+                  "$ref": "#/components/responses/Conflict",
+                },
+                "412": Object {
+                  "$ref": "#/components/responses/PreconditionFailed",
+                },
+                "500": Object {
+                  "$ref": "#/components/responses/Error",
+                },
+              },
+              "summary": "Creates or updates resources based on the natural key values of the supplied resource.",
+              "tags": Array [
+                "schoolyeartypes",
+              ],
+            },
+          },
+        },
         "servers": Array [
           Object {
             "url": "http://localhost:5198/",
