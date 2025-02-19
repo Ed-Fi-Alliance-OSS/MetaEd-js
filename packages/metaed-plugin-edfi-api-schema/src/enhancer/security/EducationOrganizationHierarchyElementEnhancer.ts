9683aac5f3a43e6a57d44e3116defaa06b992a2c
Adam Hopkins <adam.hopkins@simpat.tech>
2025-02-19 14:16:47 -0600

    Hierarchy logic and updated tests

--- packages/metaed-plugin-edfi-api-schema/src/enhancer/security/EducationOrganizationHierarchyElementEnhancer.ts
+++ packages/metaed-plugin-edfi-api-schema/src/enhancer/security/EducationOrganizationHierarchyElementEnhancer.ts
@@ -1,17 +1,34 @@
-import { EnhancerResult, MetaEdEnvironment, Namespace } from '@edfi/metaed-core';
-import { EducationOrganizationHierarchy } from '../../model/EducationOrganizationHierarchy';
+import { EnhancerResult, MetaEdEnvironment, Namespace, TopLevelEntity } from '@edfi/metaed-core';
 
 export function enhance(metaEd: MetaEdEnvironment): EnhancerResult {
+  // Hardcoded find of data standard EducationOrganization
+  const edfiEducationOrganization: TopLevelEntity | undefined = metaEd.namespace
+    .get('EdFi')
+    ?.entity.domainEntity.get('EducationOrganization');
+  if (edfiEducationOrganization == null) {
+    throw new Error(
+      'EducationOrganizationHierarchyElementEnhancer: Fatal Error: EducationOrganization not found in EdFi Data Standard project',
+    );
+  }
+
+  const allEducationOrganizations: TopLevelEntity[] = [...edfiEducationOrganization.subclassedBy, edfiEducationOrganization];
+
   metaEd.namespace.forEach((namespace: Namespace) => {
     namespace.data.educationOrganizationHierarchy = {
-      EducationOrganization: [
-        'School',
-        'LocalEducationAgency',
-        'EducationServiceCenter',
-        'CommunityOrganization',
-        'CommunityProvider',
-      ],
-    } as EducationOrganizationHierarchy;
+      EducationOrganization: allEducationOrganizations.map((edOrg) => edOrg.metaEdName),
+    };
+
+    allEducationOrganizations.forEach((edOrgType) => {
+      edOrgType.properties
+        .filter((p) => p.type === 'domainEntity' && p.parentEntity.baseEntity === edfiEducationOrganization)
+        .forEach((p) => {
+          if (namespace.data.educationOrganizationHierarchy[p.metaEdName] === undefined) {
+            namespace.data.educationOrganizationHierarchy[p.metaEdName] = [edOrgType.metaEdName];
+          } else {
+            namespace.data.educationOrganizationHierarchy[p.metaEdName].push(edOrgType.metaEdName);
+          }
+        });
+    });
   });
   return {
     enhancerName: 'EducationOrganizationHierarchyElementEnhancer',
