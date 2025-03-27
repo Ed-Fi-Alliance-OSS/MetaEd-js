af360efa0e733b72753bf4e568975bc4f3e948e2
Adam Hopkins <adam.hopkins@simpat.tech>
2025-03-27 15:08:25 -0500

    Refactor enhancer logic and add tests

--- packages/metaed-plugin-edfi-api-schema/src/enhancer/security/StudentSecurableAuthorizationEnhancer.ts
+++ packages/metaed-plugin-edfi-api-schema/src/enhancer/security/StudentSecurableAuthorizationEnhancer.ts
@@ -3,45 +3,34 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-import { MetaEdEnvironment, EnhancerResult, TopLevelEntity, ReferentialProperty, SimpleProperty } from '@edfi/metaed-core';
+import { MetaEdEnvironment, EnhancerResult, getAllEntitiesOfType, MetaEdPropertyFullName } from '@edfi/metaed-core';
 import { JsonPath } from '../../model/api-schema/JsonPath';
 import { EntityApiSchemaData } from '../../model/EntityApiSchemaData';
-import { JsonPathsInfo, JsonPathPropertyPair } from '../../model/JsonPathsMapping';
-
-function recursiveEnhance(studentReferenceProperty: ReferentialProperty | SimpleProperty) {
-  // Needs to be part of identity
-  if (!studentReferenceProperty.isPartOfIdentity) return;
-
-  // Skip role named properties
-  if (studentReferenceProperty.roleName !== '') return;
-
-  const result: Set<JsonPath> = new Set();
-
-  const { allJsonPathsMapping } = studentReferenceProperty.parentEntity.data.edfiApiSchema as EntityApiSchemaData;
-
-  Object.values(allJsonPathsMapping).forEach((jsonPathsInfo: JsonPathsInfo) => {
-    jsonPathsInfo.jsonPathPropertyPairs.forEach((jsonPathPropertyPair: JsonPathPropertyPair) => {
-      // We are only interested in the identity properties relating to the Student entity
-      if (jsonPathPropertyPair.flattenedIdentityProperty.identityProperty.parentEntityName === 'Student')
-        result.add(jsonPathPropertyPair.jsonPath);
-    });
-  });
-  (studentReferenceProperty.parentEntity.data.edfiApiSchema as EntityApiSchemaData).studentSecurableAuthorizationElements = [
-    ...result,
-  ].sort();
-
-  studentReferenceProperty.parentEntity.inReferences.forEach((referenceProperty) => {
-    recursiveEnhance(referenceProperty);
-  });
-}
+import { JsonPathsInfo } from '../../model/JsonPathsMapping';
 
 export function enhance(metaEd: MetaEdEnvironment): EnhancerResult {
-  const edfiStudent: TopLevelEntity | undefined = metaEd.namespace.get('EdFi')?.entity.domainEntity.get('Student');
-  if (edfiStudent) {
-    edfiStudent.inReferences.forEach((studentReferenceProperty) => {
-      recursiveEnhance(studentReferenceProperty);
-    });
-  }
+  getAllEntitiesOfType(metaEd, 'domainEntity', 'association', 'domainEntitySubclass', 'associationSubclass').forEach(
+    (entity) => {
+      // Using Set to remove duplicates
+      const result: Set<JsonPath> = new Set();
+
+      const { identityFullnames, allJsonPathsMapping } = entity.data.edfiApiSchema as EntityApiSchemaData;
+
+      identityFullnames.forEach((identityFullname: MetaEdPropertyFullName) => {
+        const matchingJsonPathsInfo: JsonPathsInfo = allJsonPathsMapping[identityFullname];
+        // Add student securable authorization elements
+        matchingJsonPathsInfo.jsonPathPropertyPairs.forEach((jppp) => {
+          if (
+            jppp.flattenedIdentityProperty.identityProperty.parentEntity.metaEdName === 'Student' &&
+            jppp.flattenedIdentityProperty.identityProperty.parentEntity.namespace.namespaceName === 'EdFi'
+          )
+            result.add(jppp.jsonPath);
+        });
+      });
+
+      (entity.data.edfiApiSchema as EntityApiSchemaData).studentSecurableAuthorizationElements = [...result].sort();
+    },
+  );
 
   return {
     enhancerName: 'StudentSecurableAuthorizationEnhancer',
