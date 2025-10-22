32152fb96598ce6b83e581f72a7a9eac78a50e76
Johnny Brenes <johnny.brenes@simpat.tech>
2025-10-22 10:48:30 -0600

    code review fixes for metaed plugin edfi odsapi

--- packages/metaed-plugin-edfi-odsapi/src/index.ts
+++ packages/metaed-plugin-edfi-odsapi/src/index.ts
@@ -6,21 +6,13 @@
 import type { MetaEdPlugin } from '@edfi/metaed-core';
 import { validatorList } from './validator/ValidatorList';
 import { enhancerList } from './enhancer/EnhancerList';
-import { generate as DomainMetadataGenerator } from './generator/domainMetadata/DomainMetadataGenerator';
 import { generate as ApiModelGenerator } from './generator/apiModel/ApiModelGenerator';
-import { generate as EducationOrganizationReferenceMetadataGenerator } from './generator/educationOrganizationReferenceMetadata/EducationOrganizationReferenceMetadataGenerator';
-import { generate as InterchangeOrderMetadataGenerator } from './generator/interchangeOrderMetadata/InterchangeOrderMetadataGenerator';
 
 export function initialize(): MetaEdPlugin {
   return {
     validator: validatorList(),
     enhancer: enhancerList(),
-    generator: [
-      ApiModelGenerator,
-      DomainMetadataGenerator,
-      EducationOrganizationReferenceMetadataGenerator,
-      InterchangeOrderMetadataGenerator,
-    ],
+    generator: [ApiModelGenerator],
     shortName: 'edfiOdsApi',
     configurationSchemas: new Map(),
   };
--- packages/metaed-plugin-edfi-odsapi/src/validator/ValidatorList.ts
+++ packages/metaed-plugin-edfi-odsapi/src/validator/ValidatorList.ts
@@ -5,16 +5,12 @@
 
 import { Validator } from '@edfi/metaed-core';
 
-import { validate as includingRequiredPropertiesInExtendedAssociationsProhibited } from './UpcomingImprovements/IncludingRequiredPropertiesInExtendedAssociationsProhibited';
-import { validate as includingRequiredPropertiesInExtendedDomainEntitiesProhibited } from './UpcomingImprovements/IncludingRequiredPropertiesInExtendedDomainEntitiesProhibited';
 import { validate as subclassingAnyAssociationExceptGeneralStudentProgramAssociationIsUnsupported } from './UpcomingImprovements/SubclassingAnyAssociationExceptGeneralStudentProgramAssociationIsUnsupported';
 import { validate as subclassingAnyDomainEntityExceptEducationOrganizationIsUnsupported } from './UpcomingImprovements/SubclassingAnyDomainEntityExceptEducationOrganizationIsUnsupported';
 import { validate as odsApiIgnoresRequiredOnChoicePropertiesInExtensions } from './OdsApiIgnoresRequiredOnChoicePropertiesInExtensions';
 
 export function validatorList(): Validator[] {
   return [
-    includingRequiredPropertiesInExtendedAssociationsProhibited,
-    includingRequiredPropertiesInExtendedDomainEntitiesProhibited,
     odsApiIgnoresRequiredOnChoicePropertiesInExtensions,
     subclassingAnyAssociationExceptGeneralStudentProgramAssociationIsUnsupported,
     subclassingAnyDomainEntityExceptEducationOrganizationIsUnsupported,
