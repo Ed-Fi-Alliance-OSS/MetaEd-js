9db8ecdec7d5cfdac7bfd4d1284ea62caade664f
Johnny Brenes <johnny.brenes@simpat.tech>
2025-10-22 11:19:27 -0600

    code review fixes for metaed plugin edfi ods changequery postgresql  and sqlserver

--- packages/metaed-plugin-edfi-ods-changequery-sqlserver/src/enhancer/DeleteTrackingTableCreator.ts
+++ packages/metaed-plugin-edfi-ods-changequery-sqlserver/src/enhancer/DeleteTrackingTableCreator.ts
@@ -10,72 +10,10 @@ import {
   hasRequiredNonIdentityNamespaceColumn,
   newDeleteTrackingTable,
 } from '@edfi/metaed-plugin-edfi-ods-changequery';
-import { MetaEdEnvironment, PluginEnvironment, versionSatisfies } from '@edfi/metaed-core';
+import { MetaEdEnvironment } from '@edfi/metaed-core';
 import { changeDataColumnsFor, hardcodedOldColumnFor, TARGET_DATABASE_PLUGIN_NAME } from './EnhancerHelper';
 
-function createDeleteTrackingTableModelV3dot3(table: Table): DeleteTrackingTable {
-  const tableName = `${table.schema}_${table.data.edfiOdsSqlServer.tableName}_TrackedDelete`;
-
-  const changeVersionColumn: Column = {
-    ...newColumn(),
-    columnId: 'ChangeVersion',
-    data: { edfiOdsSqlServer: { columnName: 'ChangeVersion', dataType: 'bigint' } },
-    isNullable: false,
-  };
-
-  const deleteTrackingTable: DeleteTrackingTable = {
-    ...newDeleteTrackingTable(),
-    schema: 'changes',
-    tableName,
-    primaryKeyName: `PK_${tableName}`,
-    columns: [...getPrimaryKeys(table, TARGET_DATABASE_PLUGIN_NAME)],
-    primaryKeyColumns: [changeVersionColumn],
-  };
-
-  deleteTrackingTable.columns.push({
-    ...newColumn(),
-    columnId: 'Id',
-    data: { edfiOdsSqlServer: { columnName: 'Id', dataType: 'uniqueidentifier' } },
-    isNullable: false,
-  });
-
-  deleteTrackingTable.columns.push(changeVersionColumn);
-
-  return deleteTrackingTable;
-}
-
-function createDeleteTrackingTableModelV3dot4(table: Table): DeleteTrackingTable {
-  const trackingTableName: string = table.data.edfiOdsSqlServer.tableName;
-
-  const changeVersionColumn: Column = {
-    ...newColumn(),
-    columnId: 'ChangeVersion',
-    data: { edfiOdsSqlServer: { columnName: 'ChangeVersion', dataType: 'bigint' } },
-    isNullable: false,
-  };
-
-  const deleteTrackingTable: DeleteTrackingTable = {
-    ...newDeleteTrackingTable(),
-    schema: `tracked_deletes_${table.schema}`,
-    tableName: trackingTableName,
-    primaryKeyName: `PK_${trackingTableName}`,
-    columns: [...getPrimaryKeys(table, TARGET_DATABASE_PLUGIN_NAME)],
-    primaryKeyColumns: [changeVersionColumn],
-  };
-
-  deleteTrackingTable.columns.push({
-    ...newColumn(),
-    columnId: 'Id',
-    data: { edfiOdsSqlServer: { columnName: 'Id', dataType: 'uniqueidentifier' } },
-    isNullable: false,
-  });
-
-  deleteTrackingTable.columns.push(changeVersionColumn);
-
-  return deleteTrackingTable;
-}
-
-function createDeleteTrackingTableModelV6dot0(table: Table): DeleteTrackingTable {
+export function createDeleteTrackingTableModel(_metaEd: MetaEdEnvironment, table: Table): DeleteTrackingTable {
   const trackingTableName: string = table.data.edfiOdsSqlServer.tableName;
 
   const changeVersionColumn: Column = {
@@ -111,18 +49,4 @@ function createDeleteTrackingTableModelV6dot0(table: Table): DeleteTrackingTable
   deleteTrackingTable.columns.push(changeVersionColumn);
 
   return deleteTrackingTable;
-}
-
-export function createDeleteTrackingTableModel(metaEd: MetaEdEnvironment, table: Table): DeleteTrackingTable {
-  const { targetTechnologyVersion } = metaEd.plugin.get('edfiOdsRelational') as PluginEnvironment;
-
-  if (versionSatisfies(targetTechnologyVersion, '<3.4.0')) {
-    return createDeleteTrackingTableModelV3dot3(table);
-  }
-
-  if (versionSatisfies(targetTechnologyVersion, '<6.0.0')) {
-    return createDeleteTrackingTableModelV3dot4(table);
-  }
-
-  return createDeleteTrackingTableModelV6dot0(table);
 }
--- packages/metaed-plugin-edfi-ods-changequery-sqlserver/src/enhancer/DeleteTrackingTriggerCreator.ts
+++ packages/metaed-plugin-edfi-ods-changequery-sqlserver/src/enhancer/DeleteTrackingTriggerCreator.ts
@@ -10,40 +10,10 @@ import {
   newDeleteTrackingTrigger,
 } from '@edfi/metaed-plugin-edfi-ods-changequery';
 import { Table, Column } from '@edfi/metaed-plugin-edfi-ods-relational';
-import { MetaEdEnvironment, PluginEnvironment, versionSatisfies } from '@edfi/metaed-core';
+import { MetaEdEnvironment } from '@edfi/metaed-core';
 import { changeDataColumnsWithHardcodesFor, TARGET_DATABASE_PLUGIN_NAME } from './EnhancerHelper';
 
-function createDeleteTrackingTriggerModelV3dot3(table: Table): DeleteTrackingTrigger {
-  return {
-    ...newDeleteTrackingTrigger(),
-    triggerSchema: table.schema,
-    triggerName: `${table.schema}_${table.data.edfiOdsSqlServer.tableName}_TR_DeleteTracking`,
-    targetTableSchema: table.schema,
-    targetTableName: table.data.edfiOdsSqlServer.tableName,
-    deleteTrackingTableSchema: 'changes',
-    deleteTrackingTableName: `${table.schema}_${table.data.edfiOdsSqlServer.tableName}_TrackedDelete`,
-    primaryKeyColumnNames: getPrimaryKeys(table, TARGET_DATABASE_PLUGIN_NAME).map(
-      (column: Column) => column.data.edfiOdsSqlServer.columnName,
-    ),
-  };
-}
-
-function createDeleteTrackingTriggerModelV3dot4(table: Table): DeleteTrackingTrigger {
-  return {
-    ...newDeleteTrackingTrigger(),
-    triggerSchema: table.schema,
-    triggerName: `${table.schema}_${table.data.edfiOdsSqlServer.tableName}_TR_DeleteTracking`,
-    targetTableSchema: table.schema,
-    targetTableName: table.data.edfiOdsSqlServer.tableName,
-    deleteTrackingTableSchema: `tracked_deletes_${table.schema}`,
-    deleteTrackingTableName: table.data.edfiOdsSqlServer.tableName,
-    primaryKeyColumnNames: getPrimaryKeys(table, TARGET_DATABASE_PLUGIN_NAME).map(
-      (column: Column) => column.data.edfiOdsSqlServer.columnName,
-    ),
-  };
-}
-
-function createDeleteTrackingTriggerModelV6dot0(table: Table): DeleteTrackingTrigger {
+export function createDeleteTrackingTriggerModel(_metaEd: MetaEdEnvironment, table: Table): DeleteTrackingTrigger {
   return {
     ...newDeleteTrackingTrigger(),
     triggerSchema: table.schema,
@@ -62,18 +32,4 @@ function createDeleteTrackingTriggerModelV6dot0(table: Table): DeleteTrackingTri
     omitDiscriminator: table.schema === 'edfi' && table.tableId === 'SchoolYearType',
     includeNamespace: hasRequiredNonIdentityNamespaceColumn(table),
   };
-}
-
-export function createDeleteTrackingTriggerModel(metaEd: MetaEdEnvironment, mainTable: Table): DeleteTrackingTrigger {
-  const { targetTechnologyVersion } = metaEd.plugin.get('edfiOdsRelational') as PluginEnvironment;
-
-  if (versionSatisfies(targetTechnologyVersion, '<3.4.0')) {
-    return createDeleteTrackingTriggerModelV3dot3(mainTable);
-  }
-
-  if (versionSatisfies(targetTechnologyVersion, '<6.0.0')) {
-    return createDeleteTrackingTriggerModelV3dot4(mainTable);
-  }
-
-  return createDeleteTrackingTriggerModelV6dot0(mainTable);
 }
